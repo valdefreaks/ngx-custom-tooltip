d119010c6822fcbf47e3e0fcf55d8ee6962a1550
Valdemar Farina <valdefreaks92@gmail.com>
2025-12-10 09:52:40 -0600

    fix: configure ignoreFiles in stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,4 +3,11 @@ module.exports = {
   plugins: ['stylelint-order'],
   extends: ['stylelint-config-standard', 'stylelint-config-alphabetical-order'],
   rules: {},
+  ignoreFiles: [
+    'dist/**',
+    'node_modules/**',
+    '.husky/**',
+    '.idea/**',
+    '.angular/**',
+  ],
 };
